a00bfd827716fb7fe094110d41b51dd828e15571
KodYazanDruid <45048634+KodYazanDruid@users.noreply.github.com>
2019-02-24 15:16:29 +0300

    Update armstrongsayisi.java

--- Armstrong Sayısı/armstrongsayisi.java
+++ Armstrong Sayısı/armstrongsayisi.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class armstrongsayisi {
     public static void main(String[] args){
         int t, a, b, c, temp;
-        System.out.println("3 basamaklý bir sayý giriniz: ");
+        System.out.println("3 basamakli bir sayi giriniz: ");
         t = new Scanner (System.in).nextInt();
         c = t%10;
         b = ((t-c)/10)%10;
         a = ((t-(b+c))/100)%100;
         temp = (a*a*a) + (b*b*b) + (c*c*c);
         if(t == temp){
-            System.out.println("Armstrong sayýsý!");
+            System.out.println("Armstrong sayisi!");
         }else{
-            System.out.println("Armstrong sayýsý deðil :(");
+            System.out.println("Armstrong sayisi degil :(");
         }
     }
 }
