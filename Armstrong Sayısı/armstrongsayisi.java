ab5a408b45611dfc9f9371a4833a3abf52f9e833
KodYazanDruid <mr.azix007@gmail.com>
2019-02-24 15:14:06 +0300

    Create armstrongsayisi.java

---
+++ Armstrong Sayısı/armstrongsayisi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class armstrongsayisi {
+    public static void main(String[] args){
+        int t, a, b, c, temp;
+        System.out.println("3 basamaklý bir sayý giriniz: ");
+        t = new Scanner (System.in).nextInt();
+        c = t%10;
+        b = ((t-c)/10)%10;
+        a = ((t-(b+c))/100)%100;
+        temp = (a*a*a) + (b*b*b) + (c*c*c);
+        if(t == temp){
+            System.out.println("Armstrong sayýsý!");
+        }else{
+            System.out.println("Armstrong sayýsý deðil :(");
+        }
+    }
+}
